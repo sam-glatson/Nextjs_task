bf6a6ddcd08d62b6b594397472547b2b49f4e25a
sam-glatson <samglatson2000@gmail.com>
2023-04-10 16:55:40 +0530

    padding corression

--- src/components/Profile/ProfileDetail/TabContent/infosection.tsx
+++ src/components/Profile/ProfileDetail/TabContent/infosection.tsx
@@ -7,7 +7,7 @@ import React from "react";
 const InfoSection = () => {
     return (
         <div className="w-full lg:max-w-[530px]">
-            <div className="bg-white rounded-20 sticky top-48 px-4 sm:px-8 pt-33 pb-7 sm:pb-16.5 space-y-30 shadow-4xl">
+            <div className="bg-white rounded-20 sticky top-48 px-4 sm:px-35 pt-33 pb-7 sm:pb-16.5 space-y-30 shadow-4xl">
                 <div>
                     <h3 className="text-masala-500 font-bold text-lg leading-7.5">Info</h3>
                     <hr className="mt-6.5" />
@@ -38,16 +38,16 @@ const InfoSection = () => {
                     <strong className="text-shark-550">Interests</strong>
                 </div>
                 <div className="space-x-3 inline-block leading-10 break-all">
-                    <span className="ml-3 bg-gallery-300 py-2 px-6 rounded-[10px] whitespace-nowrap text-sm leading-17 text-dune-500 ">
+                    <span className="ml-3 bg-gallery-300 py-2 px-6 rounded-10 whitespace-nowrap text-sm leading-17 text-dune-500 ">
                         Dermatology Nursing
                     </span>
-                    <span className=" bg-gallery-300 py-2 px-6 rounded-[10px] whitespace-nowrap text-sm leading-17 text-dune-500 ">
+                    <span className=" bg-gallery-300 py-2 px-6 rounded-10 whitespace-nowrap text-sm leading-17 text-dune-500 ">
                         Emergency Nursing
                     </span>
-                    <span className=" bg-gallery-300 py-2 px-6 rounded-[10px] whitespace-nowrap text-sm leading-17 text-dune-500 ">
+                    <span className=" bg-gallery-300 py-2 px-6 rounded-10 whitespace-nowrap text-sm leading-17 text-dune-500 ">
                         Infection Control
                     </span>
-                    <span className=" bg-gallery-300 py-2 px-6 rounded-[10px] whitespace-nowrap text-sm leading-17 text-dune-500 ">
+                    <span className=" bg-gallery-300 py-2 px-6 rounded-10 whitespace-nowrap text-sm leading-17 text-dune-500 ">
                         Lactation Consultation
                     </span>
                 </div>
